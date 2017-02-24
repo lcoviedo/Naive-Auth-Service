7598513e09a118cf586601486ff2b9aa2ecf8316
lcoviedo <psxlco@notingham.ac.uk>
2017-02-24 15:08:18 +0000

    Upgraded to Mirage3.0

--- config.ml
+++ config.ml
@@ -1,20 +1,21 @@
 open Mirage
 
-let stack = generic_stackv4 default_console tap0
-let auth_data = generic_kv_ro "data"
+let stack = generic_stackv4 default_network
 let http_srv = http_server @@ conduit_direct ~tls:false stack
+let auth_data = generic_kv_ro "data"
 
-let port =
-  let doc = Key.Arg.info ~doc:"Listening port." ["port"] in
-  Key.(create "port" Arg.(opt int 8080 doc))
-
-let main =
-  let libraries = [ "re.str"; "magic-mime"; "mirage-logs" ] in
-  let packages = [ "re"; "magic-mime"; "mirage-logs" ] in
-  let keys = [ Key.abstract port ] in
-  foreign
-    ~libraries ~packages ~keys
-    "Unikernel.Main" (console @-> clock @-> kv_ro @-> http @-> job)
-
+let http_port =
+  let doc = Key.Arg.info ~doc:"Listening HTTP port." ["http"] in
+  Key.(create "http_port" Arg.(opt int 8080 doc))
+	
+let main = 
+	let packages = [
+		package "re"; package "magic-mime"; package "mirage-logs"
+  ] in
+	let keys = [ Key.abstract http_port ] in
+	foreign
+	  ~packages ~keys
+		"Unikernel.Main" (mclock @-> kv_ro @-> http @-> job)
+		
 let () =
-  register "auth_service" [main $ default_console $ default_clock $ auth_data $ http_srv]
+	register "auth_service" [main $ default_monotonic_clock $ auth_data $ http_srv]
--- unikernel.ml
+++ unikernel.ml
@@ -2,6 +2,10 @@ open Lwt
 open Lwt.Infix
 open Printf
 
+(** Common signature for http and https. *)
+module type HTTP = Cohttp_lwt.Server
+
+(* Logging *)
 let server_src = Logs.Src.create "server" ~doc:"HTTP server"
 module Server_log = (val Logs.src_log server_src : Logs.LOG)
 
@@ -10,24 +14,24 @@ let string_of_stream stream =
   let s = List.map Cstruct.to_string stream in
   return (String.concat "" s)
 
-module Main (C:V1_LWT.CONSOLE) (Clock:V1.CLOCK) (FS:V1_LWT.KV_RO) (S:Cohttp_lwt.Server) = struct
-  module Logs_reporter = Mirage_logs.Make(Clock)
+module Main (MClock: Mirage_types.MCLOCK) (KV: Mirage_kv_lwt.RO) (S: HTTP) = struct 
 
-  let start console _clock fs http =
-    Logs.(set_level (Some Info));
-    Logs_reporter.(create () |> run) @@ fun () ->
-    
+  let start _clock kv http = 
     let check_pwd u p =
       Server_log.info (fun f -> f "Check password for '%s' is '%s'" u p);
-      FS.read fs u 0 4096 >>= fun readpwd ->
+      KV.read kv u 0L 4096L	>>= fun readpwd ->
       match readpwd with
-      | `Ok pwds ->
+			| Ok pwds ->
          string_of_stream pwds >>= fun pwd ->
-         Server_log.info (fun f -> f "Found password for '%s': '%s'" u (String.trim pwd));
          if String.compare (String.trim pwd) p == 0
-         then return true
-         else return false
-      | _ -> return false
+         then (
+					 Server_log.info (fun f -> f "Found password for '%s': '%s'" u (String.trim pwd)); 
+				   return true)
+				 else (
+					 Server_log.info (fun f -> f "Invalid login:\n user:'%s' and password:'%s' do not match\n" u p); 
+					 return false)
+			| Error e ->
+				(Server_log.info (fun f -> f "Error reading credentials"); return false)
     in
     let callback (_, cid) request _body =
       let uri = Cohttp.Request.uri request in
@@ -50,7 +54,7 @@ module Main (C:V1_LWT.CONSOLE) (Clock:V1.CLOCK) (FS:V1_LWT.KV_RO) (S:Cohttp_lwt.
       let cid = Cohttp.Connection.to_string cid in
       Server_log.info (fun f -> f "[%s] closing" cid);
     in
-    let port = Key_gen.port () in
+    let port = Key_gen.http_port () in
     Server_log.info (fun f -> f "listening on %d/TCP" port);
     http (`TCP port) (S.make ~conn_closed ~callback ())
 
